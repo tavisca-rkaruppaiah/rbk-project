0c15216ff57bbc391dce271feaa0732ccf534dd9
Ragu Balagi Karuppaiah <rkaruppaiah@tavisca.in>
2020-11-12 10:56:17 +0530

    added and passed the data to another component

--- src/app/reciepes/reciepe-list/reciepe-item/reciepe-item.component.ts
+++ src/app/reciepes/reciepe-list/reciepe-item/reciepe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Reciepe } from '../../model/reciepe.model';
 
 @Component({
   selector: 'app-reciepe-item',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReciepeItemComponent implements OnInit {
 
+  @Input() reciepe : Reciepe;
   constructor() { }
 
   ngOnInit(): void {
