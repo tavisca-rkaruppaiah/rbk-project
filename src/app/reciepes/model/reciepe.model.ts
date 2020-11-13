6e16f8191bd4b567bbece271e5a6d3590a3bc217
Ragu Balagi Karuppaiah <rkaruppaiah@tavisca.in>
2020-11-13 09:56:54 +0530

    added ingredients to reciepe

--- src/app/reciepes/model/reciepe.model.ts
+++ src/app/reciepes/model/reciepe.model.ts
@@ -1,5 +1,5 @@
+import { Ingredient } from 'src/app/shared/model/ingredient.model';
+
 export class Reciepe{
-    constructor(public name:string, public description:string, public imagePath:string){
-        
-    }
+    constructor(public name:string, public description:string, public imagePath:string, public ingredients : Ingredient[]){}
 }
